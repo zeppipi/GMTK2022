92d0cab80d64beb56e18f9b10e0f77b56a113505
Intecial <ljuliano30@gmail.com>
2022-07-16 12:07:24 +0800

    Created a Dice Class

---
+++ Dice Turn Based game/Assets/Scripts/Dice.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice : MonoBehaviour {
+    private ArrayList arLst;
+    void Start()
+    {
+        arLst = new ArrayList();
+        arLst.Add(1);
+        arLst.Add(2);
+        arLst.Add(3);
+        arLst.Add(4);
+        arLst.Add(5);
+        arLst.Add(6);
+    }
+    
+    // Rolls the Dice
+    public int roll(int listSize){
+        int ranIndex = UnityEngine.Random.Range(1, listSize);
+        return (int) arLst[ranIndex];
+    }
+
+    // Adds Chances on the Dice
+    public void addChances(int chance){
+        arLst.Add(chance);
+    }
+}
