7efd6cf05a59051f656477e352ab7a8755d6a9b3
Intecial <ljuliano30@gmail.com>
2022-07-17 20:40:17 +0800

    Added multiple new enemies
    Pawn, Assassin, Priest, BombSlime

---
+++ Dice Turn Based game/Assets/Scripts/Actors/BombSlime.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombSlime : BaseEnemyScript
+{
+    [SerializeField]
+    private int pounceDamage;
+    [SerializeField]
+    private int explosionDamage;
+    public override string attack(int rolls, int turns){
+        int ran = UnityEngine.Random.Range(1, 2);
+        if (ran == 1){
+            pounce();
+            return "Bomb Slime use Pounce";
+        } else {
+            explode();
+            return "Bomb Slime explodes";
+        }
+    }
+
+    private void pounce(){
+        playerScript.hurt(pounceDamage);
+    }
+
+    private void explode(){
+        playerScript.hurt(explosionDamage);
+        this.hurt(explosionDamage - (int) (explosionDamage * 0.8));
+    }
+}
---
+++ Dice Turn Based game/Assets/Scripts/Actors/Priest.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Priest : BaseEnemyScript
+{
+    [SerializeField]
+    private int healNumber;
+
+    [SerializeField]
+    private int holyAttackDamage;
+
+    public override string attack(int rolls, int turns){
+        if(rolls % 2 == 0){
+            purify();
+            holyAttack();
+            return "Priest heals and attacks you";
+        } else {
+            holyAttack();
+            return "Priest use Holy Attack";
+        }
+    }
+    
+    private void purify(){
+        this.healthAdder(healNumber);
+    }
+
+    private void holyAttack(){
+        playerScript.hurt(holyAttackDamage);
+    }
+}
